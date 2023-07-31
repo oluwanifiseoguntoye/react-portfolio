974dde8f013ff21318ccf1c7e11b448f51470b45
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-07-31 15:25:51 -0400

    Dark mode implimented

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,20 @@ import { useState } from 'react';
 
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div>
+    <div className={darkMode ? 'dark' : ""}>
       <Head>
         <title>Nifise</title>
       </Head>
       
-      <main className="bg-white px-10 md:px-20 lg:px-40">
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className= "min-h-screen">
-            <nav className="py-10 mb-12 flex justify-between">
+            <nav className="py-10 mb-12 flex justify-between dark:text-white">
               <h1 className='text-xl font-burtons'>Oluwanifise Oguntoye</h1>
               <ul className='flex items-center'>
                 <li>
-                  <BsFillMoonStarsFill className='curser-pointer text-2xl'/>
+                  <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='curser-pointer text-2xl'/>
                 </li>
                 <li>
                   <a className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
@@ -28,14 +29,14 @@ export default function Home() {
               </ul>
             </nav>
             <div className='text-center p-10'>
-              <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Oluwanifise Oguntoye</h2>
-              <h3 className='text-2xl py-2 md:text-3xl'>University of Toronto Student</h3>
-              <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto'>
+              <h2 className='text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl'>Oluwanifise Oguntoye</h2>
+              <h3 className='text-2xl py-2 dark:text-white md:text-3xl'>University of Toronto Student</h3>
+              <p className='text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-xl mx-auto'>
                   I'm a Mathematics and Statistics Double Major. I have experience with Software Development 
                   and I aspire to get my Masters in Data Science.
               </p>
             </div>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillGithub />
               <AiFillLinkedin />
             </div>
@@ -43,8 +44,8 @@ export default function Home() {
 
         <section>
           <div className='mb-90'>
-             <h3 className="text-3xl py-1">About Me</h3>
-             <p className='text-md py-2 leading-8 text-gray-800'>
+             <h3 className="text-3xl py-1 dark:text-white">About Me</h3>
+             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
               I started my undergraduate degree as an Accounting major but realized it wasn't 
               the right fit for me so while finding a new career path, I decided to learn python.
               Although I started exploring software development relatively late, 
@@ -58,7 +59,7 @@ export default function Home() {
 
         <section>
           <div>
-            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <h3 className='text-3xl py-1 dark:text-white mt-20'>Portfolio</h3>
           </div> 
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
 
@@ -71,7 +72,26 @@ export default function Home() {
             </div>
 
           </div>
+        </section>
 
+        <section>
+          <div className='mt-10'>
+            <h3 className="text-3xl py-1 dark:text-white">Get in Touch</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Feel free to reach out to me below with any
+              <span className='text-teal-500'> work opportunities</span> and 
+              <span className='text-teal-500'> project inquries</span> and I will
+              get back to you as soon as possible.
+             </p>
+          </div>
+        </section>
+
+        <section>
+          <footer className='mt-20 text-center'>
+              <h3 className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+                  2023 Oluwanifise Oguntoye
+              </h3>
+          </footer>
         </section>
 
       </main>
